f0f1cd27007eb1eb01b088646243bb7e4fbf9dfa
Kurbanov Nikita <18000056@edu.spbgasu.ru>
2021-09-24 10:26:42 +0000

    1

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+            <meta charset="UTF-8"/>
+		<title>
+			ОГО!
+		</title>
+		<link rel="StyleSheet" href="newStyleSheet.css"/>
+	</head>
+	<body>
+		<div class="static">
+			Блок 1
+		</div>
+		<div class="fixed">
+			Блок 2
+		</div>
+		<div class="float">
+			Блок 3
+		</div>
+                <div class="relative">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>    
+		</div>
+                <div class="parentOfFloat">
+			<div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div class="clear"></div>
+		</div>
+	</body>
+</html>
+
