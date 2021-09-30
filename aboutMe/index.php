f18194bf17a9f3a3b9dc082438f6d266a704961a
Kurbanov Nikita <18000056@edu.spbgasu.ru>
2021-09-30 16:57:31 +0000

    A

---
+++ aboutMe/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>
+            Обо мне!
+        </title>
+        <link rel="stylesheet" href="StylesAboutMe.css"/>
+    </head>
+    <body>
+        <div class="pageWrapper">
+            <div class="white">
+                <div class="head"> 
+                    Курбанов Никита Антонович
+                </div>
+                <div class="menu">
+                    <a href="a">Резюме</a>
+                    <a href="a">Портфолио</a>
+                    <a href="a">Проекты</a>
+                    <a href="a" class="activeLink">Обо мне</a>
+                    <a href="a">Контакты</a>
+                </div>
+                <div class="title">
+                    Обо мне
+                </div>
+                <div class="contentAboutMe">
+                    <div class="myImage">
+                        <img src="photo.png" title="Мое фото"/>
+                    </div>
+                    <div class="textAboutMe">
+                        <div class="fewWords">
+                            <span>
+                                Несколько слов обо мне
+                            </span>	
+                        </div>
+                        <p>
+                            Осмий открыт в 1803 году английским химиком Смитсоном Теннантом в сотрудничестве с Уильямом Х. Уолластоном в осадке, остающемся после растворения платины в царской водке. Сходные исследования проводились французскими химиками Колле-Дескоти, Антуаном де Фуркруа и Вокленом, которые тоже пришли к выводу о содержании неизвестного элемента в нерастворимом остатке платиновой руды. Гипотетическому элементу было присвоено имя птен (греч. πτηνός — крылатый), однако опыты Теннанта продемонстрировали, что это смесь двух элементов — иридия и осмия.
+                            Порошок осмия при нагревании реагирует с кислородом, галогенами, парами серы, селеном, теллуром, фосфором, азотной и серной кислотами. Компактный осмий не взаимодействует ни с кислотами, ни со щелочами, но с расплавами щелочей образует водорастворимые осматы.
+                        </p>
+                        <div class="quote">
+                            <p>
+                                "Название осмия происходит от др.-греч. ὀσμή «запах», так как химические реакции растворения щелочного сплава осмиридия в воде сопровождаются выделением неприятного запаха тетраоксида осмия OsO4"
+                            </p>
+                            <p>
+                                Курбанов Никита, <b>студент группы ПМИ-4</b>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="skills">
+                    <div class="mySkills">
+                        <div class="mySkillsTitle">
+                            Мои навыки
+                        </div>
+                        <div class="Matlab">Matlab</div>
+                        <div class="Windows">Windows</div>
+                        <div class="Offices">Offices</div>
+                        <div class="html">HTML</div>
+                        <div class="css">css</div>
+                        <div class="line">
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                            <div>4</div>
+                            <div>5</div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                    <div class="canIDo">
+                        <div class="canIDoTitle">
+                                Что я могу
+                        </div>
+                        <ul class="canIDoList">
+                            <li>
+                                Программировать
+                            </li>
+                            <li>
+                                Копировать
+                            </li>
+                            <li>
+                                Вставлять
+                            </li>
+                            <li>
+                                Сохранять
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="reviews"></div>
+            </div>
+            <div class="black">
+                <div class="footer">
+                    <div class="footerCols">
+                        <div class="addres">
+                            2-я Красноармейская ул., 4,<br/> 
+                            Санкт-Петербург, 190005
+                        </div>
+                        <div class="contacts">
+                            Общий отдел: 8 (812) 575-05-34, <br/>
+                            Почта: rector@spbgasu.ru, <br/>
+                            Приемная комиссия: (812) 316-20-26
+                        </div>
+                        <div class="socialLinks">
+                            <a href="https://www.facebook.com/SPbGASU/">Facebook</a><br/>
+                            <a href="https://vk.com/spbgasu_vk">VK</a><br/>
+                            <a href="https://www.instagram.com/spbgasu_official/">Instagram</a><br/>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                    <div>
+                        <div class="footerMenu">
+                            <a href="a">Резюме</a>
+                            <a href="a">Портфолио</a>
+                            <a href="a">Проекты</a>
+                            <a href="a" class="activeLink">Обо мне</a>
+                            <a href="a">Контакты</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
